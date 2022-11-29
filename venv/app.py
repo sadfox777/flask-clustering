55a599b880da0311cc1d3a8fb8fe621b7e329e72
danangwisnup <danangwisnu@students.undip.ac.id>
2022-11-29 23:58:47 +0700

    update dataset

--- venv/app.py
+++ venv/app.py
@@ -4,55 +4,45 @@ import flask
 import pickle
 from flask import Flask, redirect, url_for, request, render_template
 
-
-# creating instance of the class
+# Membuat instance dari kelas
 app = Flask(__name__, template_folder='templates')
-
-# to tell flask what url should trigger the function index()
-
 
 @app.route('/')
 @app.route('/index')
 def index():
     return flask.render_template('index.html')
 
-
-# prediction function
+# Membuat fungsi untuk memprediksi
 def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1, 2)
+    to_predict = np.array(to_predict_list).reshape(1, 4)
     loaded_model = pickle.load(
-        open("./model/model.pkl", "rb"))  # load the model
-    # predict the values using loded model
+        open("./model/model.pkl", "rb")) # Load "model.pkl"
+    # Memprediksi nilai menggunakan model yang dimuat
     result = loaded_model.predict(to_predict)
     return result[0]
 
-
+# Membuat fungsi untuk mengambil nilai dari form
 @app.route('/result', methods=['POST'])
 def result():
     if request.method == 'POST':
-        name = request.form['name']
-        gender = request.form['gender']
-        age = request.form['age']
-        height = request.form['height']
-        weight = request.form['weight']
-
-        to_predict_list = list(map(float, [height, weight]))
+        total_cases = request.form['total_cases']
+        total_active_cases = request.form['total_active_cases']
+        population = request.form['population']
+        total_deaths = request.form['total_deaths']
+        
+        to_predict_list = list(map(float, [total_cases, total_active_cases, population, total_deaths]))
         result = ValuePredictor(to_predict_list)
 
-        #CLuster Index 
-        #0 = Obesitas
-        #1 = Tidak Obesitas
-        #2 = Normal
-        
+        # Hasil clustering
         if float(result) == 0:
-            prediction = 'You are Obesitas'
+            clustering = 'Cluster 0: Termasuk dalam populasi jumlahnya palang yang rendah, total kasus paling rendah, total kasus aktif paling rendah, dan total kematian paling rendah.'
         elif float(result) == 1:
-            prediction = 'You are not Obesitas'
-        elif float(result) == 2:
-            prediction = 'You are Normal'
+            clustering = 'Cluster 1: Termasuk dalam populasi yang tinggi, tingkat kematian yang tinggi, dan tingkat kasus aktif yang tinggi.'
+        else:
+            clustering = 'Cluster 2: Termasuk dalam populasi yang sedang, tingkat kematian sedang, dan tingkat kasus aktif sedang.'
 
 
-        return render_template("result.html", prediction=prediction, name=name)
+        return render_template("result.html", clustering=clustering)
 
 
 if __name__ == "__main__":
